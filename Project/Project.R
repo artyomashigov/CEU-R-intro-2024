4bca1040f90e97a3656e4e3e7c2c449a08cad76b
artyomashigov <artyomashigov@gmail.com>
2024-01-29 13:47:39 +0100

    "add the structure of the project"

--- Project/Project.R
+++ Project/Project.R
@@ -7,6 +7,23 @@ install.packages("curl")
 # import data, we use fread to get a data.table
 locations = fread('https://raw.githubusercontent.com/artyomashigov/CEU-R-intro-2024/main/Project/Data/locations.csv')
 locations
-# exploratory analysis and cleaning
+
 str(locations)
 
+#import make details
+make_details = fread('https://raw.githubusercontent.com/artyomashigov/CEU-R-intro-2024/main/Project/Data/make_details.csv')
+make_details
+
+stolen_vehicles = fread('https://raw.githubusercontent.com/artyomashigov/CEU-R-intro-2024/main/Project/Data/stolen_vehicles.csv')
+stolen_vehicles
+
+# exploratory analysis and cleaning
+
+
+
+
+
+
+
+
+# visualization

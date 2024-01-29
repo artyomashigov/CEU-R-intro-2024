05c2d044ceee3cddf823f100e5736ec02b8247c6
artyomashigov <artyomashigov@gmail.com>
2024-01-29 01:09:10 +0100

    import locations

--- Project.R
+++ Project.R
@@ -1,3 +1,12 @@
-# import data
+# import packages
+library(data.table)
+# to use fread
+install.packages("curl")
 
+
+# import data, we use fread to get a data.table
+locations = fread('https://raw.githubusercontent.com/artyomashigov/CEU-R-intro-2024/main/Project/Data/locations.csv')
+locations
 # exploratory analysis and cleaning
+str(locations)
+
